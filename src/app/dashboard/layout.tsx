3ff899fa074cb89636730f73134b044e7252bae8
NeilRow56 <neilrowland56@gmail.com>
2024-08-13 09:52:45 +0100

    first commit- Kinde, dashboard and Navigation

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,99 @@
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { CircleUser, MenuIcon } from "lucide-react";
+import { ReactNode } from "react";
+
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ThemeToggle } from "@/components/dashboard/ThemeToggle";
+import { DashboardNavigation } from "@/components/dashboard/DashboardNavigation";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/");
+  }
+  return (
+    <div className="flex flex-col w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b ">
+        <nav className="hidden font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <DashboardNavigation />
+        </nav>
+        <div className="flex items-center gap-4">
+          <ThemeToggle />
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                className="shrink-0 md:hidden"
+                variant="outline"
+                size="icon"
+              >
+                <MenuIcon className="h-5 w-5" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left">
+              <nav className="flex flex-col gap-6 text-lg font-medium mt-5">
+                <DashboardNavigation />
+              </nav>
+            </SheetContent>
+          </Sheet>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <Avatar>
+                  <AvatarImage src={user.picture || ""} />
+                  <AvatarFallback>{user.given_name?.charAt(0)}</AvatarFallback>
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel className="font-normal">
+                <div className="flex flex-col space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    {user.given_name}
+                  </p>
+                  <p className="text-xs leading-none text-muted-foreground">
+                    {user.email}
+                  </p>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  Settings
+                  <DropdownMenuShortcut>TODO</DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                <LogoutLink>Logout</LogoutLink>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </header>
+
+      <main className="my-5">{children}</main>
+    </div>
+  );
+}
---
+++ src/components/dashboard/DashboardNavigation.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const links = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    name: "Manage",
+    href: "/dashboard/manage",
+  },
+  {
+    name: "Categories",
+    href: "/dashboard/categories",
+  },
+  {
+    name: "Transactions",
+    href: "/dashboard/transactions",
+  },
+];
+
+export function DashboardNavigation() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          className={cn(
+            link.href === pathname
+              ? "text-foreground font-bold text-lg border-b-4 border-sky-500"
+              : "text-muted-foreground hover:text-foreground text-lg"
+          )}
+        >
+          {link.name}
+        </Link>
+      ))}
+    </>
+  );
+}
