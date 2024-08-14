e6efc38d30db21fbd78b74c11e39c7447026da3e
NeilRow56 <neilrowland56@gmail.com>
2024-08-14 09:53:01 +0100

    GET request api created for categories- query added to CategoryPicker component

--- src/components/dashboard/CategoryPicker.tsx
+++ src/components/dashboard/CategoryPicker.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { TransactionType } from "@/lib/transactionTypes";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 interface CategoryPickerProps {
@@ -10,5 +13,10 @@ export default function CategoryPicker({
   type,
   onChange,
 }: CategoryPickerProps) {
+  const categoriesQuery = useQuery({
+    queryKey: ["categories", type],
+    queryFn: () =>
+      fetch(`/api/categories?type=${type}`).then((res) => res.json()),
+  });
   return <div>CategoryPicker</div>;
 }
