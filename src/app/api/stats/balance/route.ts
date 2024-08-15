03cbecdadb25db415ce5ce072ce694f2cb4c8d88
NeilRow56 <neilrowland56@gmail.com>
2024-08-15 17:00:22 +0100

    Added GET route for transaction totals in Overview. Added OverviewQuerySchems

---
+++ src/app/api/stats/balance/route.ts
@@ -0,0 +1,60 @@
+import db from "@/lib/db";
+import { OverviewQuerySchema } from "@/schemas/overview";
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+import { redirect } from "next/navigation";
+
+export async function GET(request: Request) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/");
+  }
+
+  const { searchParams } = new URL(request.url);
+  const from = searchParams.get("from");
+  const to = searchParams.get("to");
+
+  const queryParams = OverviewQuerySchema.safeParse({ from, to });
+
+  if (!queryParams.success) {
+    return Response.json(queryParams.error.message, {
+      status: 400,
+    });
+  }
+
+  const stats = await getBalanceStats(
+    user.id,
+    queryParams.data.from,
+    queryParams.data.to
+  );
+
+  return Response.json(stats);
+}
+
+export type GetBalanceStatsResponseType = Awaited<
+  ReturnType<typeof getBalanceStats>
+>;
+
+async function getBalanceStats(userId: string, from: Date, to: Date) {
+  const totals = await db.transaction.groupBy({
+    by: ["type"],
+    where: {
+      userId,
+      date: {
+        gte: from,
+        lte: to,
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  return {
+    expense: totals.find((t) => t.type === "expense")?._sum.amount || 0,
+    income: totals.find((t) => t.type === "income")?._sum.amount || 0,
+  };
+}
