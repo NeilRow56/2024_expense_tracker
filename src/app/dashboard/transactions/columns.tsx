08bd59b3cf940f250da87253b5e3d95e7cff92e2
NeilRow56 <neilrowland56@gmail.com>
2024-08-18 07:47:26 +0100

    Added SORTING to table

--- src/app/dashboard/transactions/columns.tsx
+++ src/app/dashboard/transactions/columns.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import Link from "next/link";
 import { GetTransactionHistoryResponseType } from "@/app/api/transactions-history/route";
+import { DataTableColumnHeader } from "@/components/datatable/ColumnHeader";
 
 // This type is used to define the shape of our data. We have used the type from the get function and have selected one element.
 // You can use a Zod schema here if you want.
@@ -22,6 +23,9 @@ type TransactionHistoryRow = GetTransactionHistoryResponseType[0];
 export const columns: ColumnDef<TransactionHistoryRow>[] = [
   {
     accessorKey: "category",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Category" />
+    ),
 
     cell: ({ row }) => (
       <div className="flex gap-2 capitalize">
--- src/components/dashboard/TransactionTable.tsx
+++ src/components/dashboard/TransactionTable.tsx
@@ -4,6 +4,8 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
   useReactTable,
 } from "@tanstack/react-table";
 import {
@@ -17,8 +19,9 @@ import {
 import { GetTransactionHistoryResponseType } from "@/app/api/transactions-history/route";
 import { DateToUTCDate } from "@/lib/helpers";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 import { columns } from "@/app/dashboard/transactions/columns";
+import SkeletonWrapper from "../SkeletonWrapper";
 
 interface Props {
   from: Date;
@@ -28,6 +31,8 @@ interface Props {
 const emptyData: any[] = [];
 
 export default function TransactionTable({ from, to }: Props) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const history = useQuery<GetTransactionHistoryResponseType>({
     queryKey: ["transactions", "history", from, to],
     queryFn: () =>
@@ -42,52 +47,70 @@ export default function TransactionTable({ from, to }: Props) {
     data: history.data || emptyData,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
   });
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
+    <div className="w-full">
+      <div className="flex flex-wrap items-end justify-between gap-2">
+        TODO: FILTERS
+      </div>
+      <SkeletonWrapper isLoading={history.isFetching}>
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
                         )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
                   </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </SkeletonWrapper>
     </div>
   );
 }
