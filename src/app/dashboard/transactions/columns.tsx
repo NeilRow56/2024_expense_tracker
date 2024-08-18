97182e664f83e19e0e68a8301386a0ddfb3e0b7f
NeilRow56 <neilrowland56@gmail.com>
2024-08-18 07:27:20 +0100

    Transaction Table - second stage

---
+++ src/app/dashboard/transactions/columns.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { GetTransactionHistoryResponseType } from "@/app/api/transactions-history/route";
+
+// This type is used to define the shape of our data. We have used the type from the get function and have selected one element.
+// You can use a Zod schema here if you want.
+
+type TransactionHistoryRow = GetTransactionHistoryResponseType[0];
+
+export const columns: ColumnDef<TransactionHistoryRow>[] = [
+  {
+    accessorKey: "category",
+
+    cell: ({ row }) => (
+      <div className="flex gap-2 capitalize">
+        {row.original.categoryIcon}
+        <div className="capitalize">{row.original.category}</div>
+      </div>
+    ),
+  },
+];
--- src/components/dashboard/TransactionTable.tsx
+++ src/components/dashboard/TransactionTable.tsx
@@ -1,14 +1,31 @@
 "use client";
 
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { GetTransactionHistoryResponseType } from "@/app/api/transactions-history/route";
 import { DateToUTCDate } from "@/lib/helpers";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { columns } from "@/app/dashboard/transactions/columns";
 
 interface Props {
   from: Date;
   to: Date;
 }
+
+const emptyData: any[] = [];
 
 export default function TransactionTable({ from, to }: Props) {
   const history = useQuery<GetTransactionHistoryResponseType>({
@@ -21,6 +38,56 @@ export default function TransactionTable({ from, to }: Props) {
       ).then((res) => res.json()),
   });
 
-  console.log(history);
-  return <div>TransactionTable</div>;
+  const table = useReactTable({
+    data: history.data || emptyData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
