2e32afcbc581743f0286e35cd8e1458464ce13f2
NeilRow56 <neilrowland56@gmail.com>
2024-08-18 08:01:25 +0100

    Added extra columns to Table

--- src/app/dashboard/transactions/columns.tsx
+++ src/app/dashboard/transactions/columns.tsx
@@ -14,6 +14,7 @@ import {
 import Link from "next/link";
 import { GetTransactionHistoryResponseType } from "@/app/api/transactions-history/route";
 import { DataTableColumnHeader } from "@/components/datatable/ColumnHeader";
+import { cn } from "@/lib/utils";
 
 // This type is used to define the shape of our data. We have used the type from the get function and have selected one element.
 // You can use a Zod schema here if you want.
@@ -32,6 +33,59 @@ export const columns: ColumnDef<TransactionHistoryRow>[] = [
         {row.original.categoryIcon}
         <div className="capitalize">{row.original.category}</div>
       </div>
+    ),
+  },
+  {
+    accessorKey: "description",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Description" />
+    ),
+
+    cell: ({ row }) => (
+      <div className="capitalize">{row.original.description}</div>
+    ),
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+    cell: ({ row }) => {
+      const date = new Date(row.original.date);
+      const formattedDate = date.toLocaleDateString("default", {
+        timeZone: "UTC",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      });
+      return <div className="text-muted-foreground">{formattedDate}</div>;
+    },
+  },
+  {
+    accessorKey: "type",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Type" />
+    ),
+    cell: ({ row }) => (
+      <div
+        className={cn(
+          "rounded-lg p-2 text-center capitalize",
+          row.original.type === "income" &&
+            "bg-emerald-400/10 text-emerald-500",
+          row.original.type === "expense" && "bg-red-400/10 text-red-500"
+        )}
+      >
+        {row.original.type}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "amount",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Amount" />
+    ),
+    cell: ({ row }) => (
+      <p className="text-md rounded-lg bg-gray-400/5 p-2 text-right font-medium">
+        {row.original.formattedAmount}
+      </p>
     ),
   },
 ];
